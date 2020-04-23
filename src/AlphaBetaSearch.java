f3eb12ae1594d9980e121898d43262523d8ae3d1
James <jlansdown@gmail.com>
2020-04-22 17:39:34 -0700

    Testing with java.util.timer added. Now returns board with new move after 5 seconds

--- src/AlphaBetaSearch.java
+++ src/AlphaBetaSearch.java
@@ -1,6 +1,23 @@
+import java.util.*;
+
+
 public class AlphaBetaSearch {
 
-    Board state;
+    Board curState;
+    int MAX_DEPTH = 3;
 
+    AlphaBetaSearch(Board state) {
+        this.curState = state;
+    }
+
+    Move search() {
+        Move move = new Move(5,5);
+
+        //Iterative Deepening
+        for (int depth = 1; depth < MAX_DEPTH; depth++) {
+
+        }
+        return move;
+    }
 
 }
--- src/Isolation.java
+++ src/Isolation.java
@@ -1,25 +1,44 @@
-public class Isolation {
+import java.util.*;
+
+public class Isolation{
+
+    int secondsPassed = 0;
+    Move aiMove;
+    Board board;
+
+    Isolation() {
+        board = new Board();
+    }
 
 
+    TimerTask task = new TimerTask() {
+        public void run() {
+            secondsPassed++;
+            if(secondsPassed > 5) {
+                System.out.println("Took too long");
+                System.exit(-1);
+            } else {
+                board.updateBoard(board.player1, aiMove);
+                System.out.println(board);
+                System.exit(0);
+            }
+        }
+    };
 
 
+    public void getAiMove() {
+        System.out.println(board);
+        Timer timer = new Timer();
+        timer.schedule(task, 5*1000);
+        aiMove = new Move(5, 5);
+
+    }
 
 
+    public static void main(String args[]) {
+        Isolation isolation = new Isolation();
 
-
-
-    public static void main(String args[]){
-        Board a = new Board();
-        System.out.println(a);
-        Move move = new Move(5,5);
-
-        if(a.isValidMove(a.player1, move))
-            a.updateBoard(a.player1, move);
-
-
-        System.out.println();
-        System.out.println(a);
-
+        isolation.getAiMove();
     }
 
 
