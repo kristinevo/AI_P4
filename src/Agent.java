a4a4a8127d49655b812a903e280410eac7ecbf75
kristinevo <kmvo@cpp.edu>
2020-04-26 15:02:37 -0700

    + generate successors wasn't going farther than the neighbors so I fixed that + added half of the heuristic

--- src/Agent.java
+++ src/Agent.java
@@ -2,12 +2,13 @@ import java.util.ArrayList;
 
 public class Agent extends Player{
 
-    Player opponent;
-    private int MAX_DEPTH = 3;
+    final double INFINITY = Double.MAX_VALUE;
+    final double NEGATIVE_INFINITY = Double.MIN_VALUE;
+    final private int MAX_DEPTH = 3;
     private int current_depth = 0;
-    double infinity = Double.MAX_VALUE;
-    double negativeInfinity = Double.MIN_VALUE;
-    Move ai_next_move = new Move(0,0);
+
+    private Player opponent;
+    private Move ai_next_move = new Move(0,0);
 
     Agent(int turn, Player opponent) {
         super();
@@ -23,16 +24,17 @@ public class Agent extends Player{
         }
 
         this.opponent = opponent;
-        MAX_DEPTH = 3;
     }
 
     private Move aplhaBetaSearch(Board state) {
         ArrayList<Move> successors = generateSuccessors(state);
-        double v, best_v = infinity;
-        double alpha = negativeInfinity;
+        double v, best_v = INFINITY;
+        double alpha = NEGATIVE_INFINITY;
         current_depth++;
 
-        //TODO: if there are no moves able to be generated, that's called game over
+        if(successors.isEmpty()){
+            return new Move(-1,-1);
+        }
 
         for(Move i: successors) {
 
@@ -49,15 +51,11 @@ public class Agent extends Player{
 
     double maxValue(Board state, double alpha, double beta) {
         //TODO: have a function which calculates the score of the state
-        if(current_depth == MAX_DEPTH)
-            return 0;
-
-        //TODO: probably smart to make it return as the MAX_SCORE
-        if (terminalTest(state))
+        if(current_depth == MAX_DEPTH || terminalTest(state))
             return 0;
 
         current_depth++;
-        double value = negativeInfinity;
+        double value = NEGATIVE_INFINITY;
         ArrayList<Move> successors = generateSuccessors(state);
 
         for (int s = 0; s < successors.size(); s++) {
@@ -73,15 +71,11 @@ public class Agent extends Player{
 
     double minValue(Board state, double alpha, double beta) {
         //TODO: have a function which calculates the score of the state
-        if(current_depth == MAX_DEPTH)
-            return 0;
-
-        //TODO: probably smart to make it return as the MAX_SCORE
-        if (terminalTest(state))
+        if(current_depth == MAX_DEPTH || terminalTest(state))
             return 0;
 
         current_depth++;
-        double value = infinity;
+        double value = INFINITY;
         ArrayList<Move> successors = generateSuccessors(state);
 
         for (int s = 0; s < successors.size(); s++) {
@@ -95,8 +89,40 @@ public class Agent extends Player{
         return value;
     }
 
-    double heuristic_function(){
+    double heuristic_function(Board board, Move move){
         //TODO: pythagorean distance from center + (moves ai has - moves opponent has)
+        double agent_distance = 0;
+        Move agent_temp_pos = new Move(this.getXPosition(), this.getYPosition());
+        Move opponent_temp_pos = new Move(opponent.getXPosition(), opponent.getYPosition());
+        int agent_moves = 0;
+        int opponent_moves = 0;
+
+        //distance: lower number, the better
+        agent_distance += Math.abs(move.getY() - 0) / Math.abs(move.getX() - 0);
+        agent_distance += Math.abs(move.getY() - 0) / Math.abs(move.getX() - 7);
+        agent_distance += Math.abs(move.getY() - 7) / Math.abs(move.getX() - 0);
+        agent_distance += Math.abs(move.getY() - 7) / Math.abs(move.getX() - 7);
+        agent_distance /= 4;
+
+        //moves
+        /*if(board.isValidMove(this,  this.move_up()))
+            successors.add(this.move_up());
+        if(board.isValidMove(this,  this.move_down()))
+            successors.add(this.move_down());
+        if(board.isValidMove(this,  this.move_left()))
+            successors.add(this.move_left());
+        if(board.isValidMove(this,  this.move_right()))
+            successors.add(this.move_right());
+        if(board.isValidMove(this,  this.move_diagonal_up_left()))
+            successors.add(this.move_diagonal_up_left());
+        if(board.isValidMove(this,  this.move_diagonal_up_right()))
+            successors.add(this.move_diagonal_up_right());
+        if(board.isValidMove(this,  this.move_diagonal_down_right()))
+            successors.add(this.move_diagonal_down_right());
+        if(board.isValidMove(this,  this.move_diagonal_down_left()))
+            successors.add(this.move_diagonal_down_left());
+        */
+
         return 0;
     }
 
@@ -117,24 +143,56 @@ public class Agent extends Player{
     ArrayList<Move> generateSuccessors(Board board){
         int i = 1;
         ArrayList<Move> successors = new ArrayList<Move>();
+        Move up = new Move(this.getXPosition(), this.getYPosition());
+        Move down = new Move(this.getXPosition(), this.getYPosition());
+        Move left = new Move(this.getXPosition(), this.getYPosition());
+        Move right = new Move(this.getXPosition(), this.getYPosition());
+        Move up_right = new Move(this.getXPosition(), this.getYPosition());
+        Move up_left = new Move(this.getXPosition(), this.getYPosition());
+        Move down_right = new Move(this.getXPosition(), this.getYPosition());
+        Move down_left = new Move(this.getXPosition(), this.getYPosition());
 
-        while(this.getXPosition() + i < 7 || this.getXPosition() - i > 0 || this.getYPosition() + i < 7 || this.getXPosition() - i > 0){
-            if(board.isValidMove(this,  this.move_up()))
-                successors.add(this.move_up());
-            if(board.isValidMove(this,  this.move_down()))
-                successors.add(this.move_down());
-            if(board.isValidMove(this,  this.move_left()))
-                successors.add(this.move_left());
-            if(board.isValidMove(this,  this.move_right()))
-                successors.add(this.move_right());
-            if(board.isValidMove(this,  this.move_diagonal_up_left()))
-                successors.add(this.move_diagonal_up_left());
-            if(board.isValidMove(this,  this.move_diagonal_up_right()))
-                successors.add(this.move_diagonal_up_right());
-            if(board.isValidMove(this,  this.move_diagonal_down_right()))
-                successors.add(this.move_diagonal_down_right());
-            if(board.isValidMove(this,  this.move_diagonal_down_left()))
-                successors.add(this.move_diagonal_down_left());
+        while(true){
+            up.setX(up.getX() - i);
+            down.setX(up.getX() + i);
+            right.setY(right.getY() + i);
+            left.setY(left.getY() - i);
+            up_left.setX(up_left.getX() - i);
+            up_left.setY(up_left.getY() - i);
+            up_right.setX(up_right.getX() - i);
+            up_right.setY(up_right.getY() + i);
+            down_left.setX(down_left.getX() + i);
+            down_left.setY(down_left.getY() - i);
+            down_right.setX(down_right.getX() + i);
+            down_right.setY(down_left.getY() + i);
+
+            if(board.isValidMove(this, up))
+                successors.add(up);
+
+            else if(board.isValidMove(this, down))
+                successors.add(down);
+
+            else if(board.isValidMove(this, left))
+                successors.add(left);
+
+            else if(board.isValidMove(this,  right))
+                successors.add(right);
+
+            else if(board.isValidMove(this, up_left))
+                successors.add(up_left);
+
+            else if(board.isValidMove(this,  up_right))
+                successors.add(up_right);
+
+            else if(board.isValidMove(this, down_right))
+                successors.add(down_right);
+
+            else if(board.isValidMove(this, down_left))
+                successors.add(down_left);
+
+            else
+                break;
+
             i++;
         }
 
--- src/Move.java
+++ src/Move.java
@@ -9,4 +9,6 @@ class Move {
 
     int getX() {return x;}
     int getY() {return y;}
+    void setX(int x) { this.x = x; }
+    void setY(int y) { this.y = y; }
 }
